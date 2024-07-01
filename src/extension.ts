132a2cce2995205f18f5a4d4af7f2d19e746dbbc
DudaGod <dudkevich@yandex-team.ru>
2024-07-01 12:45:32 +0300

    chore: rename from vscode-hermione to vscode-testplane

--- src/extension.ts
+++ src/extension.ts
@@ -4,19 +4,19 @@ export function activate(context: vscode.ExtensionContext): void {
     const disposable = vscode.commands.registerCommand("hrm.install", async () => {
         const [workspaceFolder] = vscode.workspace.workspaceFolders || [];
         if (!workspaceFolder) {
-            await vscode.window.showErrorMessage("Open a folder in VS Code to initialize Hermione");
+            await vscode.window.showErrorMessage("Open a folder in VS Code to initialize Testplane");
             return;
         }
 
         const terminal = vscode.window.createTerminal({
-            name: "Install Hermione",
+            name: "Install Testplane",
             cwd: workspaceFolder.uri.fsPath,
             env: process.env,
         });
 
         terminal.show();
 
-        terminal.sendText(`npm init hermione-app -- --yes`, true);
+        terminal.sendText(`npm init testplane@latest -- --yes`, true);
     });
 
     context.subscriptions.push(disposable);
